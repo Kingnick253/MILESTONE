3ccc61cce7475e705a97f9ff86467d834656ff9a
alkasah <alkasah485@gmail.com>
2022-09-17 10:37:18 -0700

    updation in signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,14 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#username').value.trim();
+    //const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password').value.trim();
   
-    if (email && password && username) {
+    if (username && password ) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ email, password, username }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
